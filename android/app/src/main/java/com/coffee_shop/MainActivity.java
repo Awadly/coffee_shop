6145bb0c4807bc1d91dd6fa487c7ac9dbaa9205b
Awadly <elawadas@whitman.edu>
2023-11-14 13:12:32 +0200

    adding assets

--- android/app/src/main/java/com/coffee_shop/MainActivity.java
+++ android/app/src/main/java/com/coffee_shop/MainActivity.java
@@ -1,5 +1,5 @@
 package com.coffee_shop;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -15,6 +15,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "coffee_shop";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
